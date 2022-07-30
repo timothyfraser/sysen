ae69a7fcd4da0ff3d96a1e864e2bc8e6593945ea
Timothy Fraser, PhD <timothy.fraser.1@gmail.com>
2022-07-30 01:42:15 +0000

    Test commit

---
+++ vault/packages.R
@@ -0,0 +1,29 @@
+# Install main classroom use packages
+install.packages(
+  c("tidyverse",
+    "broom",
+    "devtools",
+    
+    # Visualization packages
+    "viridis",
+    "ggtext",
+    "shadowtext",
+    
+    # Statistical packages
+    "mtvnorm",
+    "PearsonDS",
+    "texreg"))
+
+# Install extra packages for visualization and workshop development
+install.packages(
+  c("magick",
+    "rsvg",
+    "fontawesome",
+    "cowplot",
+    "knitr",
+    "kableExtra",
+    "rmdformats",
+    "secret")
+)
+
+# Install more packages
