5bdef6bee79dafca391befeffb342941bc494f59
Timothy Fraser, PhD <timothy.fraser.1@gmail.com>
2022-07-31 20:27:58 +0000

    commit!

--- dev/update.R
+++ dev/update.R
@@ -6,19 +6,18 @@ library(rmarkdown)
 library(rmdformats)
 
 # Unpack the zipfile into dev/prep, using the functions in "unpack.R"
-source("dev/unpack.R")
+#source("dev/unpack.R")
 
 # Set working directory as project location
 setwd(".")
 
 # Render all Workshops
-render("dev/prep/workshop_1.Rmd", output_dir = "docs")
-render("dev/prep/workshop_2.Rmd", output_dir = "docs")
-render("dev/prep/workshop_3.Rmd", output_dir = "docs")
-render("dev/prep/lesson_3.Rmd", output_dir = "docs")
+render("dev/prep/workshop_1.Rmd", output_dir = "docs"); rm(list= ls())
+render("dev/prep/workshop_2.Rmd", output_dir = "docs"); rm(list= ls())
+render("dev/prep/workshop_3.Rmd", output_dir = "docs"); rm(list= ls())
+render("dev/prep/lesson_3.Rmd", output_dir = "docs"); rm(list= ls())
+render("dev/prep/workshop_4.Rmd", output_dir = "docs"); rm(list= ls())
 
 # Pack and delete the dev/prep directory.
-source("dev/pack.R")
-
-  
+#source("dev/pack.R")
 
