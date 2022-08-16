4801b00b4356c11732b37bf3f61bd1f6337cfa1b
Timothy Fraser, PhD <timothy.fraser.1@gmail.com>
2022-08-16 23:34:28 +0000

    commit

---
+++ workshops/packages.R
@@ -0,0 +1,36 @@
+# Install main classroom use packages
+install.packages(
+  c("tidyverse",
+    "broom",
+    "devtools",
+    
+    # Visualization packages
+    "viridis",
+    "ggtext",
+    "shadowtext",
+    "DiagrammeR",
+    
+    # Statistical packages
+    "mtvnorm",
+    "PearsonDS",
+    "moments",
+    "texreg",
+    "mosaicCalc",
+    
+    # Sample data packages
+    "gapminder",
+    "nycflights",
+    "fivethirtyeight"))
+
+# Install extra packages for visualization, etc.
+install.packages(
+  c("magick",
+    "rsvg",
+    "fontawesome",
+    "cowplot",
+    "knitr",
+    "kableExtra",
+    "rmdformats",
+    "fidelius",
+    "credentials")
+)
