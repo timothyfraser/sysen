5525ed2dc305681d5d2236261c01064096cdce3a
Timothy Fraser, PhD <timothy.fraser.1@gmail.com>
2022-08-30 22:45:40 -0400

    Update workshop_2.R

--- workshops/workshop_2.R
+++ workshops/workshop_2.R
@@ -13,10 +13,11 @@ la <- read_csv("workshops/la_parishes.csv")
 
 # This data.frame contains 7 vectors for 20 Lousiana parishes:
 
-# fips: unique ID for each county (called a "parish")
+# parish: name of each county (called a "parish" in Louisiana)
+# code: unique ID 5-digit code for each county
+# coastal: (0 or 1) is parish located on coastline?
 # pc_damaged: % buildings damaged by hurricane
 # pc_severe: % buildings SEVERELY damaged by hurricane
-# coastal: (0 or 1) is parish located on coastline?
 # pc_poverty: % residents below poverty line
 # pc_nonwhite: % nonwhite residents (communities of color)
 # unemployment: unemployment rate per 1000 residents
