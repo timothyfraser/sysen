6950b3b1626b3273f327367db9d7710b47edf09b
Timothy Fraser, PhD <timothy.fraser.1@gmail.com>
2024-09-27 15:30:29 -0400

    Create 05_recitation.R

---
+++ code/05_recitation.R
@@ -0,0 +1,35 @@
+# recitation_5.R
+# Dr. Fraser
+# Recitation 5: Statistical Process Control Practice in R
+
+# 0. Setup ##################################################
+# Load packages
+library(dplyr)
+library(readr)
+library(ggplot2)
+library(ggpubr)
+
+# Load data
+water = read_csv("workshops/onsen.csv")
+
+
+# Exercise 1 ###############################################
+
+
+# Load functions
+# ***You'll need to go add this script for this to work.
+# https://github.com/timothyfraser/sysen/tree/main/functions/functions_process_control.R
+source("functions/functions_process_control.R")
+
+
+# Exercise 2 ###############################################
+
+
+# Pick a quality metric from our onsen dataset,
+# and generate averages, standard deviation, and range plots,
+# using our helper functions.
+
+# Describe the process under study.
+
+
+
