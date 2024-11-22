633ec91650a1f70b1ed8d52eb5852148eb7b782d
Timothy Fraser, PhD <timothy.fraser.1@gmail.com>
2024-11-22 16:26:30 -0500

    Update 14_workshop.R

--- code/14_workshop.R
+++ code/14_workshop.R
@@ -34,10 +34,124 @@ m0 %>% glance()
 
 
 
+cookies %>% 
+  lm(formula = yum ~ molasses + ginger + cinnamon + butter + flour) %>%
+  glance()
+
+
+# first order polynomial
+
+# y = slope*x
+
+# second order polynomial
+
+# y = slope1 * x + slope2 * x^2
+
+
+# two second order polynomials
+y = slope1*x + slope2*x^2 + slope3*z + slope4*z^2
+
+
+cor(cookies$yum, cookies$cinnamon)
+cor(cookies$yum, cookies$molasses)
+cor(cookies$yum, cookies$butter)
+cor(cookies$yum, cookies$flour)
+
+cookies %>% lm(formula = yum ~ molasses)
+
+cookies %>% lm(formula = yum ~ poly(molasses, 2) )
+# y = 18.64 +
+# 629.38 * molasses +
+# 158.90 * molasses^2
+
+cookies %>% lm(formula = yum ~ poly(molasses, 2) ) %>% glance()
+
+m = cookies %>% lm(formula = yum ~ poly(molasses, 2) + 
+                 poly(ginger, 2) +
+                 poly(cinnamon, 2) +
+                 poly(butter, 2) +
+                 poly(flour, 2)  )
+cookies %>% head()
+tibble(
+  molasses = 0.75,
+  ginger = 1,
+  cinnamon = 1,
+  butter = 0.75,
+  flour = 2.75
+) %>%
+  mutate(yhat = predict(m, newdata = .))
+
+
+m = cookies %>% lm(formula = yum ~ poly(molasses, 2) + 
+                     poly(ginger, 2) +
+                     poly(cinnamon, 2) +
+                     poly(butter, 2) +
+                     poly(flour, 2) +
+                     I(molasses * cinnamon)  )
+
+
+# Second order polynomial with interactions
+m %>% glance()
 # Interaction effects
 # Polynomials (squares)
 
 
+
+
+
+m = cookies %>% lm(
+  formula = yum ~ poly(molasses, 2) +  poly(ginger, 2) + I(molasses * ginger)  )
+glance(m)
+
+
+data = tidyr::expand_grid(
+  molasses = seq(from = 0, to = 3, by = 0.1),
+  ginger = seq(from = 0, to = 3, by = 0.1)
+) %>%
+  mutate(yhat = predict(m, newdata = .))
+
+data
+
+
+data %>%
+  filter(yhat == max(yhat))
+
+
+library(viridis)
+
+ggplot() +
+  geom_tile(data = data, mapping = aes(x = molasses, y = ginger, fill = yhat)) +
+  scale_fill_viridis(option = "plasma") +
+  # Add contours
+  geom_contour(data = data, mapping = aes(x = molasses, y = ginger, z = yhat),
+             color = "white", binwidth = 1)
+
+library(metR)
+
+ggplot() +
+  geom_contour_fill(
+    data = data, mapping = aes(x = molasses, y = ginger, z = yhat),
+                  binwidth = 1, color = "white") +
+  scale_fill_viridis(option = "plasma")
+
+ggplot() +
+  geom_contour_fill(
+    data = data, mapping = aes(x = molasses, y = ginger, z = yhat),
+    binwidth = 2, color = "white") +
+  scale_fill_viridis(option = "plasma") +
+  geom_text_contour(
+    data = data, mapping = aes(x = molasses, y= ginger, z = yhat),
+    color = "black", stroke.color = "white", stroke = 0.2,
+    skip = 0, label.placer = label_placer_n(1)
+  )
+
+
+
+
+
+
+
+# EXAMPLES FROM PAST YEARS ##########################################
 
 cookies %>% 
   lm(formula = yum ~ molasses + ginger * cinnamon + butter + flour)
