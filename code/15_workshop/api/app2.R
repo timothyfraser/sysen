a1eae8ff5c57d9789f7b094d5c60badad54658dc
Timothy Fraser, PhD <timothy.fraser.1@gmail.com>
2025-11-25 12:50:48 -0500

    Add Plumber API with data retrieval and random values
    This file implements a Plumber REST API with endpoints for generating random values and retrieving data from a Google Sheets spreadsheet.

---
+++ code/15_workshop/api/app2.R
@@ -0,0 +1,60 @@
+# app2.R
+# Example Plumber REST API - added data extraction!
+# Author: Tim Fraser
+
+# pairs with these slides: https://docs.google.com/presentation/d/1BBkr_ElCpgXijiTIwyKqxGMNjvhPJx7xEWkjh8YJfSA/edit?usp=sharing
+# pairs with this data: https://docs.google.com/spreadsheets/d/15yBf5tapFSq-n6668GQ_fa3LQNafJUtxxJdjPhbaT3c/edit?usp=sharing 
+# pairs with this form: https://forms.gle/R8axC5GAMXcm7VWd6
+
+# PACKAGES ###############################
+
+library(plumber) # for REST APIs
+library(dplyr) # for data wrangling
+library(readr) # for reading/writing data
+library(httr) # for API calls
+library(jsonlite) # for using JSONs
+
+#* @apiTitle Bahamas Bistro API
+#* @apiDescription REST API for querying our database of customer orders.
+#* @apiHost NULL
+#* @apiContact list(name = "Tim Fraser, PhD", url = "https://www.timothyfraser.com", email = "tmf77@cornell.edu")
+#* @apiVersion 1
+
+
+# ENDPOINTS ##############################
+
+## /random/ ##############################
+#* Test retrieve `n` randomly generated values
+#* @get /random/
+#* @param n:int An integer number of samples to draw from normal distribution
+#* @response 200 A vector
+#* @serializer json
+function(n = 3){
+  n = as.integer(n)
+  output = rnorm(n = n)
+  return(output)
+}
+
+
+
+## /retrieve-data/ ########################
+#* Retrieve data from spreadsheet
+#* @get /retrieve-data/
+#* @param n:int Number of most recent orders to return
+#* @response 200 A data.frame
+#* @serializer csv
+function(n = 3){
+
+  n = as.integer(n)  
+  
+  # Change share link to download link
+  url = "https://docs.google.com/spreadsheets/d/15yBf5tapFSq-n6668GQ_fa3LQNafJUtxxJdjPhbaT3c/export?format=csv"
+  
+  # Download data
+  data = url %>% 
+    read_csv() %>%
+    tail(n) %>%
+    select(Timestamp, order_id, task, team, person, done, elapsed) 
+  
+  return(data)
+}
