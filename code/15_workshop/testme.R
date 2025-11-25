34ba7773eb80b19126a19287288bf64dff6dc0f8
Timothy Fraser, PhD <timothy.fraser.1@gmail.com>
2025-11-25 13:05:13 -0500

    Update testme.R

--- code/15_workshop/testme.R
+++ code/15_workshop/testme.R
@@ -2,22 +2,40 @@
 
 # A script for testing our different products in this folder.
 
+library(shiny)
 library(plumber)
 library(rmarkdown)
 
+
+# Run shiny app v1
+shiny::runApp("code/15_workshop/dashboard/app1.R")
+
+# Run shiny app v2
+shiny::runApp("code/15_workshop/dashboard/app2.R")
+
+# Run shiny app v3
+shiny::runApp("code/15_workshop/dashboard/app3.R")
+
+# Run shiny app v4
+shiny::runApp("code/15_workshop/dashboard/app4.R")
+
+
 # Run Rmarkdown reporter
 rmarkdown::render(
-  input = "code/15_workshop/report.Rmd", 
+  input = "code/15_workshop/report/report.Rmd", 
   output_file = "report.html")
 
 
 # Run app1
-plumber::plumb(file='code/15_workshop/app1.R')$run()
+plumber::plumb(file='code/15_workshop/api/app1.R')$run()
 
 
 # Run app2
-plumber::plumb(file='code/15_workshop/app2.R')$run()
+plumber::plumb(file='code/15_workshop/api/app2.R')$run()
 
 
 # Run app3
-plumber::plumb(file='code/15_workshop/app3.R')$run()
+plumber::plumb(file='code/15_workshop/api/app3.R')$run()
+
+# Run app4
+plumber::plumb(file='code/15_workshop/api/app4.R')$run()
